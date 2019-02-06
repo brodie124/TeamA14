7890df004e534e4985e35e711d145244ae9974b0
Pestell <bp18125@essex.ac.uk>
2019-02-06 15:31:32 +0000

    Creates FeatureSelection class;

--- selection.py
+++ selection.py
@@ -1,83 +1,76 @@
+"""
+Created on Wed Feb  6 13:37:18 2019
+
+@author: bp18125
+"""
+
 import pandas
 import numpy
 
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.feature_selection import chi2
+class FeatureSelector:
+    def __init__(self, filename):
+        self.ignored_attributes = []
+        self.categorised_attributes = []
+        self.data = pandas.read_csv(filename)
+    
+    def get_attributes(self):
+        return list(self.data.columns)
+    
+    
+    def ignore_attribute(self, attribute):
+        if(type(attribute) is list):
+            self.ignored_attributes += attribute
+        else:
+            self.ignored_attributes.append(attribute)
+            
+    def categorise_attribute(self, attribute):
+        if(type(attribute) is list):
+            self.categorised_attributes += attribute
+        else:
+            self.categorised_attributes.append(attribute)
+    
+    def rank_features(self, top):  
+        for categorised in self.categorised_attributes:
+            self.data[categorised] = self.data[categorised].astype('category').cat.codes
+        
+        self.data = self.data.dropna()
+        self.data = self.data.drop(columns=self.ignored_attributes)
+    
+        data_values = self.data.values
+        
+        axisX = data_values[:,0:len(self.get_attributes())]
+        axisY = data_values[:,len(self.get_attributes())-1]
+        
+        model = ExtraTreesClassifier()
+        model.fit(axisX, axisY)
+        
+        important_features = model.feature_importances_
+        features_ranked = []
+        for i in range(0, len(important_features)):
+            features_ranked.append((i, important_features[i]))
 
-attributes = [
-    'Id','MSSubClass','MSZoning','LotFrontage','LotArea','Street','Alley',
-    'LotShape','LandContour','Utilities','LotConfig','LandSlope',
-    'Neighborhood','Condition1','Condition2','BldgType','HouseStyle',
-    'OverallQual','OverallCond','YearBuilt','YearRemodAdd','RoofStyle',
-    'RoofMatl','Exterior1st','Exterior2nd','MasVnrType','MasVnrArea',
-    'ExterQual','ExterCond','Foundation','BsmtQual','BsmtCond',
-    'BsmtExposure','BsmtFinType1','BsmtFinSF1','BsmtFinType2',
-    'BsmtFinSF2','BsmtUnfSF','TotalBsmtSF','Heating','HeatingQC',
-    'CentralAir','Electrical','1stFlrSF','2ndFlrSF','LowQualFinSF',
-    'GrLivArea','BsmtFullBath','BsmtHalfBath','FullBath','HalfBath',
-    'BedroomAbvGr','KitchenAbvGr','KitchenQual','TotRmsAbvGrd','Functional',
-    'Fireplaces','FireplaceQu','GarageType','GarageYrBlt','GarageFinish',
-    'GarageCars','GarageArea','GarageQual','GarageCond','PavedDrive',
-    'WoodDeckSF','OpenPorchSF','EnclosedPorch','3SsnPorch','ScreenPorch'
-    ,'PoolArea','PoolQC','Fence','MiscFeature','MiscVal','MoSold','YrSold'
-    ,'SaleType','SaleCondition'
-]
+        features_ranked.sort(key=lambda x: x[1], reverse=True)
+        
+        return [x[0] for x in features_ranked][0:top]
 
-dummy_attributes = [
+        
+selector = FeatureSelector('kaggle_data/train.csv')
+
+selector.categorise_attribute([
     'MSZoning', 'Street', 'Alley', 'LotShape', 'LandContour', 'Utilities',
-    'LotConfig','LandSlope','Neighborhood', 'Condition1', 'Condition2',
+    'LotConfig', 'LandSlope', 'Neighborhood', 'Condition1', 'Condition2',
     'BldgType', 'HouseStyle', 'RoofStyle', 'RoofMatl', 'Exterior1st',
     'Exterior2nd', 'MasVnrType', 'ExterQual', 'ExterCond', 'Foundation',
     'BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2',
-    'BsmtFinType2', 'Heating','HeatingQC', 'CentralAir', 'Electrical','KitchenQual',
-    'Functional','FireplaceQu','GarageType','GarageFinish','GarageQual','GarageCond',
-    'PavedDrive','PoolQC','Fence', 'MiscFeature','SaleType','SaleCondition'
-]
-
-ignored_attributes = [
-    'Id'
-]
-
-
-
-
-
-num_of_features = 40
-
-print("Training Data...")
-
-data_frame = pandas.read_csv('kaggle_data/test.csv')
-data_frame = pandas.get_dummies(data_frame, columns=dummy_attributes)
-data_frame = data_frame.dropna()
-
-data_frame.drop(columns=ignored_attributes)
-
-for v in ignored_attributes:
-    attributes.remove(v)
-
-
-# print(data_frame.na)
-data_values = data_frame.values
-
-axisX = data_values[:,0:len(attributes)]
-axisY = data_values[:,len(attributes)]
-
-
-model = ExtraTreesClassifier()
-model.fit(axisX, axisY)
-
-
-print("Data Trained")
-
-
-important_features = model.feature_importances_
-features_ranked = []
-for i in range(0, len(important_features)):
-    features_ranked.append((i, important_features[i]))
-
-features_ranked.sort(key=lambda x: x[1], reverse=True)
-
-
-print("Top " + str(num_of_features) + " Features: ")
-for i in range(0, num_of_features):
-    print("(" + str(features_ranked[i][1]) + ") " + attributes[features_ranked[i][0]])
+    'BsmtFinType2', 'Heating', 'HeatingQC', 'CentralAir', 'Electrical', 'KitchenQual',
+    'Functional', 'FireplaceQu', 'GarageType', 'GarageFinish', 'GarageQual', 'GarageCond',
+    'PavedDrive', 'PoolQC', 'Fence', 'MiscFeature', 'SaleType', 'SaleCondition'
+])
+    
+selector.ignore_attribute([
+    'Id', 'PoolQC', 'MiscFeature', 'Fence', 'GarageYrBlt', 'SalePrice'
+])
+    
+features = selector.rank_features(40)
+for f in features:
+    print(selector.data.columns[f])
